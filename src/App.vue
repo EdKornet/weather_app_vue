075d13f0528405531a371c66b54ee6c144082de9
DESKTOP-SS2MF1D\ED <kornet@greenline-software.com>
2023-10-07 02:08:50 +0300

    added main styles , added some input logic

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+  data() {
+    return {
+      city: ""
+    }
+  },
+  computed : {
+    cityName() {
+      return '"' + this.city + '"'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Weather Application </h1>
+    <p>find out the weather {{city =="" ? "in your city" : "in " + cityName}} </p>
+    <input type="text"
+           v-model="city"
+           placeholder="Enter city">
+    <button v-if="city !=='' ">get the weather</button>
+    <button v-else disabled>enter the name of the city</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background: #1f0f24;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+}
+
+.wrapper h1{
+  margin-top: 50px;
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  outline: none;
+  padding: 5px 8px;
+  transition: all .2s;
+}
+
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms  ease;
+}
+
+.wrapper button:disabled {
+  background: #746027;
+  cursor: not-allowed;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
+</style>
